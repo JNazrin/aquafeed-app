6f9d1aff31d0ffe3cac8c37cf6837c322a672c3b
JohanNazrin310 <johannazrin310@gmail.com>
2023-12-26 00:40:23 +0800

    Update main.dart

--- aquafeed-app/lib/main.dart
+++ aquafeed-app/lib/main.dart
@@ -1,13 +1,31 @@
-import 'package:aquafeed_app/src/constants/colors.dart';
-import 'package:flutter/material.dart';
+import 'package:aquafeed_app/src/bindings/general_bindings.dart';
 import 'package:aquafeed_app/src/features/welcome.dart';
-import 'package:aquafeed_app/src/features/home/screens/tips/tips_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:aquafeed_app/src/utils/constants/colors.dart';
+import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:aquafeed_app/firebase_options.dart';
+import 'package:aquafeed_app/src/data/repositories/authentication/authentication_repository.dart';
 
-void main() => runApp(MaterialApp(
-  theme: ThemeData(scaffoldBackgroundColor: whiteColor, fontFamily: "Poppins"),
-  initialRoute: '/tips',
-  routes: {
-    '/': (context) => const Welcome(),
-    '/tips': (context) => const Tips(),
-  },
-));
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Firebase & Authentication Repository
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.android).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(scaffoldBackgroundColor: whiteColor, fontFamily: "Poppins"),
+      initialBinding: GeneralBindings(),
+      home: const Welcome(),
+    );
+  }
+}
