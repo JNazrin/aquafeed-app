95653acbc8edb46fbfa7c9e6d800cb266c7e9335
JohanNazrin310 <johannazrin310@gmail.com>
2023-12-26 22:39:47 +0800

    reformat folder structure

--- aquafeed-app/lib/src/constants/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-const primaryColor = Color(0xFF2A41CB);
-const whiteColor = Color(0xFFFFFFFF);
-const textColor = Color(0xFF1D1F24);
-const textColor2 = Color(0xFF676D75);
-const boxColor = Color(0xFFF1F1F1);
